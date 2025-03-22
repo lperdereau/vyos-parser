90ba82f2d956394dee3b5befa1066c4a53f6b792
Louis PERDEREAU <leolouis.perdereau@gmail.com>
2025-03-22 20:25:23 +0100

    fix: Add type exporting

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { parseConfig } from './parser';
+import type { Config, QuotedString, Value, Section } from './parser';
 import { generateConfig } from './generator';
 
 export {
     parseConfig,
     generateConfig,
+    Config,
+    QuotedString,
+    Value,
+    Section,
 }
