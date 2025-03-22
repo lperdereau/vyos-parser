ae4cc277bb9424a0323dce6957babfbfd75d23c2
Louis PERDEREAU <leolouis.perdereau@gmail.com>
2025-03-22 18:58:04 +0100

    feat: Add test

---
+++ src/parser.test.ts
@@ -0,0 +1,54 @@
+import { parseConfig } from "./parser";
+import { expect, it } from 'vitest'
+
+const test = `
+container {
+  name ui {
+    allow-host-networks
+    environment NODE_TLS_REJECT_UNAUTHORIZED {
+      value 0
+    }
+    image ghcr.io/lperdereau/vyos-ui:v0.0.1-alpha4
+    volume socket {
+      destination /run/api.sock
+      source /run/api.sock
+    }
+  }
+}
+protocols {
+  static {
+    route 0.0.0.0/0 {
+      /* Default route */
+      next-hop 192.168.1.254
+    }
+  }
+}`;
+
+const assert = {
+  "container": {
+    "name ui": {
+      "allow-host-networks": true,
+      "environment NODE_TLS_REJECT_UNAUTHORIZED": {
+        "value": "0"
+      },
+      "image": "ghcr.io/lperdereau/vyos-ui:v0.0.1-alpha4",
+      "volume socket": {
+        "destination": "/run/api.sock",
+        "source": "/run/api.sock"
+      }
+    }
+  },
+  "protocols": {
+    "static": {
+      "route 0.0.0.0/0": {
+        "next-hop": "192.168.1.254"
+      }
+    }
+  }
+};
+
+
+it('parser', () => {
+  const parsed = parseConfig(test);
+  expect(parsed).toStrictEqual(assert);
+})
